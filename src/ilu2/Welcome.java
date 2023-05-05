e89c8d384acb58643cac997157363a62888cd116
SalimTP <salim31moussaoui@hotmail.com>
2023-05-05 23:12:04 +0200

    ITER_7

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -4,27 +4,20 @@ public class Welcome {
 
     public static String welcome(String input) {
         StringBuilder chaine = new StringBuilder("Hello, ");
-        
+
         if(input == null || input.trim().equals("")) 
             return chaine.append("my friend").toString();
-          
+
         if(input.toUpperCase().equals(input)) {
             chaine.append(input);
             return chaine.append(" !").toString().toUpperCase();
         }
-        
-        if (nomContientMajuscule(input))      	
-        	return gererNomsMajusculeMinuscule(input,chaine).toString();
-        
-        String[] noms2 = gestionPlusieursNoms(input);
-        if(noms2.length!=0) {
-            for (int i = 0;i<noms2.length-1;i++){
-                chaine.append(noms2[i]+", ");
-            }
-            chaine.append(majusculePremiereLettre(noms2[noms2.length-1]));
+        if(input.contains(",")){
+        	trieNomMajusculeMinuscule(input,chaine);
             return chaine.toString();
-        }
-        
+        	
+        }   
+
         chaine.append(majusculePremiereLettre(input));
         return chaine.toString();
     }
@@ -34,19 +27,64 @@ public class Welcome {
         return input.substring(0, 1).toUpperCase() + input.substring(1);
     }    
     
-    // Méthode permettant la gestion de deux noms en les séparant et en mettant leur première lettre en majuscule
-    private static String[] gestionPlusieursNoms(String input) {
-        if(input.contains(",")) {
-            String[] noms = input.split(",");
-            String[] nomsMajuscule = new String[noms.length];
-            for(int i=0; i<noms.length; i++) {
-                nomsMajuscule[i] = majusculePremiereLettre(noms[i].trim());
+    private static void gestionNomsMinuscule(String[] noms, int nombreMinuscule, StringBuilder chaine) {
+            int nbNoms = nombreMinuscule;
+            if (nbNoms == 1) {
+            	chaine.append(majusculePremiereLettre(noms[0].trim()));
+            } else if (nbNoms == 2) {
+            	chaine.append(majusculePremiereLettre(noms[0].trim())).append(" and ").append(majusculePremiereLettre(noms[1].trim()));
+            } else {
+                for(int i = 0; i < nbNoms - 1; i++) {
+                	if((i+1)==(nbNoms - 1))
+                		chaine.append(majusculePremiereLettre(noms[i].trim()));                		
+                	else
+                		chaine.append(majusculePremiereLettre(noms[i].trim())).append(", ");
+                }
+                chaine.append(" and ").append(majusculePremiereLettre(noms[nbNoms - 1].trim()));
             }
-            return nomsMajuscule;
-        }
-        return new String[0];
     }
-     
+    
+    private static void gestionNomsMajuscule(String[] noms, int nombreMajuscule, StringBuilder chaine) {    
+        int nbNoms = nombreMajuscule;
+        if (nbNoms == 1) {
+        	chaine.append(majusculePremiereLettre(noms[0].trim()));
+        } else if (nbNoms == 2) {
+        	chaine.append(majusculePremiereLettre(noms[0].trim())).append(" AND ").append(majusculePremiereLettre(noms[1].trim()));
+        } else {
+            for(int i = 0; i < nbNoms - 1; i++) {
+            	if((i+1)==(nbNoms - 1))
+            		chaine.append(majusculePremiereLettre(noms[i].trim()));                		
+            	else
+            		chaine.append(majusculePremiereLettre(noms[i].trim())).append(", ");
+            }
+            chaine.append(" AND ").append(majusculePremiereLettre(noms[nbNoms - 1].trim()));
+        }
+        chaine.append(" !");
+    }
+    
+    private static void trieNomMajusculeMinuscule(String input, StringBuilder chaine) {
+    	
+    	String[] noms = input.split(",");
+    	String[] nomsMinuscule = new String[noms.length];
+    	String[] nomsMajuscule = new String[noms.length];
+    	int nombreMinuscule=0, nombreMajuscule=0;    	
+    	for(int i=0;i<noms.length;i++) {
+    		if(noms[i].toUpperCase().equals(noms[i])) {   		
+    			nomsMajuscule[nombreMajuscule]=noms[i];
+    			nombreMajuscule++;
+    		}
+    		else {    			
+    			nomsMinuscule[nombreMinuscule]=noms[i];
+    			nombreMinuscule++;    			
+    		}    			
+    	}
+    	gestionNomsMinuscule(nomsMinuscule,nombreMinuscule,chaine);
+    	if(nombreMajuscule>0 && nomContientMajuscule(input)) {
+    		chaine.append(". AND HELLO, ");
+    		gestionNomsMajuscule(nomsMajuscule,nombreMajuscule,chaine);    		
+    	}	
+    }
+
     private static boolean nomContientMajuscule(String input) {
         if(input.contains(",")) {
             String[] noms = input.split(",");
@@ -57,27 +95,6 @@ public class Welcome {
         } 
         return false;
     }
-
-    private static StringBuilder gererNomsMajusculeMinuscule(String input,StringBuilder chaine) {
-        String[] noms = input.split(",");
-        String[] nomsMajuscule =  new String[noms.length];
-        String[] nomsMinuscule = new String[noms.length];
-        int j =0;
-        chaine.replace(0, chaine.length(), "Hello");
-
-        for(int i=0; i<noms.length; i++) {
-            if(noms[i].toUpperCase().equals(noms[i])) {
-            	nomsMajuscule[j]=noms[i];            	
-            	j++;
-            }                
-            else 
-            	chaine.append(", " + majusculePremiereLettre(noms[i].trim()));     
-        }
-       
-        chaine.append(". AND HELLO");
-        for(int i=0; i<j; i++) {
-            chaine.append(", " + majusculePremiereLettre(nomsMajuscule[i].trim()));
-        }
-        return chaine.append(" !");   
-    }
+   
+ 
 }
--- test/ilu2/WelcomeTest.java
+++ test/ilu2/WelcomeTest.java
@@ -25,23 +25,29 @@ class WelcomeTest {
 		assertEquals("HELLO, JULIEN !", Welcome.welcome("JULIEN"));
 		assertEquals("HELLO, BOB !", Welcome.welcome("BOB"));
 	}
-	
+
 	@Test
 	void test_ITER_4() {
-		assertEquals("Hello, Amy, Bob", Welcome.welcome("amy,bob"));
-		assertEquals("Hello, Adrien, Boudrier", Welcome.welcome("Adrien,boudrier"));
+		assertEquals("Hello, Amy and Bob", Welcome.welcome("amy,bob"));
+		assertEquals("Hello, Adrien and Boudrier", Welcome.welcome("Adrien,boudrier"));
 	}
 	
 	@Test
 	void test_ITER_5() {
-		assertEquals("Hello, Amy, Bob, John", Welcome.welcome("Amy,bob, john"));
-		assertEquals("Hello, Adrien, Boudrier, Alex", Welcome.welcome("Adrien, boudrier, Alex"));
+		assertEquals("Hello, Amy, Bob and John", Welcome.welcome("Amy,bob, john"));
+		assertEquals("Hello, Adrien, Boudrier and Alex", Welcome.welcome("Adrien, boudrier, Alex"));
 	}
 	
 	@Test
 	void test_ITER_6() {
-		assertEquals("Hello, Amy, Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
-		assertEquals("Hello, Adrien, Boudrier. AND HELLO, ALEX !", Welcome.welcome("Adrien, boudrier, ALEX"));
-		assertEquals("Hello, Adrien, Boudrier, Clara, Arley. AND HELLO, ALEX, RONE, MARCEL !", Welcome.welcome("Adrien, boudrier, ALEX, RONE, Clara,Arley,MARCEL"));
+		assertEquals("Hello, Amy and Jerry. AND HELLO, BOB !", Welcome.welcome("Amy, BOB, Jerry"));
+		assertEquals("Hello, Adrien and Boudrier. AND HELLO, ALEX !", Welcome.welcome("Adrien, boudrier, ALEX"));
+		assertEquals("Hello, Adrien, Boudrier, Clara and Arley. AND HELLO, ALEX, RONE AND MARCEL !", Welcome.welcome("Adrien, boudrier, ALEX, RONE, Clara, Arley, MARCEL"));
+	}
+	
+	@Test
+	void test_ITER_7() {
+		assertEquals("Hello, Bob, Amy and Jerry", Welcome.welcome("bob, amy, jerry"));
+		assertEquals("Hello, Bob and Jerry. AND HELLO, AMY AND JACK !", Welcome.welcome("bob, AMY, jerry, JACK"));
 	}
 }
